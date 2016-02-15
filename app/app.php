2080d22ec5cd8342606a257d626fe70299968334
erik tolentino and nic netzel <netzeln@gmail.com>
2016-02-15 09:21:06 -0800

    add interface

--- app/app.php
+++ app/app.php
@@ -1,13 +1,22 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/~~~~~~.php";
+    require_once __DIR__."/../src/TitleCaseGenerator.php";
 
-    session_start();
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get("/view_title_case", function() use ($app){
+        $my_TitleCaseGenerator = new TitleCaseGenerator;
+        $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+        return $app['twig']->render('view_title_case.html.twig', array('result'=>$title_cased_phrase));
+    });
 
 
 
